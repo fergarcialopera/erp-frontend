dfa551f65601bf687e3e583eb1a971121208ebc3
fergarcialopera <fergarcialopera@gmail.com>
2026-05-26 08:47:36 +0200

    refactor: remove suppressConsoleNoise functionality and related import
    - Deleted suppressNonErrorConsole function to simplify console logging behavior.
    - Removed import statement from main.tsx as the function is no longer utilized.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
-import { suppressNonErrorConsole } from "@/lib/suppressConsoleNoise";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
-suppressNonErrorConsole();
 
 createRoot(document.getElementById("root")!).render(<App />);
